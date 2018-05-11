5b00d7c2db5f6fd577c29615b94f79a8ebf54b3f
Thanh Nguyen <btnguyen2k@gmail.com>
2018-05-11 21:07:57 +0700

    Bug fixes

--- src/main/g8/release.sh
+++ src/main/g8/release.sh
@@ -4,14 +4,14 @@
 ## Usage:
 ##   ./release.sh <tag-name>
 
-if [ "$1" == "" ]; then
-	echo "Usage: $0 tag-name"
+if [ "\$1" == "" ]; then
+	echo "Usage: \$0 tag-name"
 	exit -1
 fi
 
-echo "$1"
-git commit -m "$1"
-git tag -f -a "$1" -m "$1"
-git push origin "$1" -f
+echo "\$1"
+git commit -m "\$1"
+git tag -f -a "\$1" -m "\$1"
+git push origin "\$1" -f
 git push
 
